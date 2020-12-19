5b7baca5994b7fc5870a225a00c41351a72cdac7
joanord97 <joanord97@gmail.com>
2020-12-19 20:36:44 +0100

    Unity Setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField]
+    private int _speed = 10;
+
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        transform.Translate(direction);
+
+        
+    }
+}
+
